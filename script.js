293c2dd9a50c0149c80053cc529ee5f407fa67fa
AmogelangTebogoShongwe <mompatitebogo03@gmail.com>
2025-06-06 00:31:35 +0200

    We keep pushing

---
+++ script.js
@@ -0,0 +1,9 @@
+// we gonna learn how to use DOM elements of the calculator
+// DOM(DOCUMENT OBJECT MODEL)
+const inputBox = document.getElementById('input');
+const expressionDiv = document.getElementById('expression');
+const resultsDiv = document.getElementById('results');
+
+//define expression and result variable
+
+let expression = 
